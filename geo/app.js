089d55720e0f6bf06272423574c943a9730ee5d6
Kristaps Karnitis <47277475+kris-dev-cloudwarelab@users.noreply.github.com>
2026-02-17 13:26:24 +0200

    rename and geo publish

---
+++ geo/app.js
@@ -0,0 +1,313 @@
+
+
+/// Set game params
+let raj_nr, attempts, secs, timer_enabled;
+let random, map_path, raj;
+
+let pre_guessed_regions = 0;
+loadLevel(1);
+
+function loadLevel(index){
+
+	/// Reset params
+	raj_nr = pre_guessed_regions;
+	attempts = 0;
+	secs = 0;
+	timer_enabled = true;
+
+	/// Load level data
+	let level = levels[index];
+	console.log("loadLevel() level", level);
+	raj = level.regions;
+
+	/// Load map
+	const obj = document.getElementById("map_svg_object");
+	obj.data = level.map_path;
+	obj.addEventListener("load", () => {
+		// setTimeout(startGame, 2000);
+		startGame();
+	});
+}
+function startGame(){
+	
+	console.log("startGame() raj", raj);
+
+	/// Sagatavot rajonus
+	for(i=0; i<raj.length; i++){
+		
+		/// Sagatavot rajona pogu
+		let id = raj[i].id;
+		let poly = getRegionPolygon(id);
+		if(poly){
+			poly.classList.add("rajons");
+			poly.setAttribute("name", raj[i].name);
+			if(raj[i].color) poly.style.fill = raj[i].color; /// Sea has predefined lightblue
+			else poly.style.fill = "var(--light-green-color)";
+
+			poly.onclick = createClickHandler(i);
+			
+			/// Sagatavot mainīgos
+			raj[i].attempts = 0;
+			raj[i].guessed = false;
+		}
+	}
+
+	/// Sajaukt rajonus
+	random = JSON.parse(JSON.stringify(raj));
+	shuffle(random);
+
+	if(pre_guessed_regions){
+		for(i=0; i<pre_guessed_regions; i++){
+			let region = random[i];
+			let region_id = region.id;
+			let nr_in_raj = raj.findIndex(function(item) { return item.id === region_id; })
+			raj[nr_in_raj].attempts = 1;
+			attempts++;
+			markRegionAsGuessed(nr_in_raj);
+		}
+	}
+
+	/// Sākt spēli
+	printNextRegionQuestion();
+	runTimer();
+
+}
+function printNextRegionQuestion(){
+	printEmoji("🤔");
+	printMsg(`Kur atrodas ${random[raj_nr].name}?`, "white");
+}
+function printEmoji(emoji){
+	big_emoji.innerHTML = emoji;
+}
+
+function createClickHandler(i) {
+  return function() { clickRaj(i); };
+}
+
+function getRegionPolygon(id){
+	const svgDoc = map_svg_object.contentDocument;
+    const element = svgDoc.getElementById(id);
+	return element;
+}
+function getById(id){
+	return document.getElementById(id);
+}
+
+
+
+
+function runTimer(){
+
+	if(timer_enabled){
+		secs = secs + 1;
+		let timer = getById("timer");
+		timer.innerHTML = printTime();
+		setTimeout(function() {
+			runTimer();
+		}, 1000);
+	}
+}
+function secondsToTime(secs)
+{
+    var hours = Math.floor(secs / (60 * 60));
+
+    var divisor_for_minutes = secs % (60 * 60);
+    var minutes = Math.floor(divisor_for_minutes / 60);
+
+    var divisor_for_seconds = divisor_for_minutes % 60;
+    var seconds = Math.ceil(divisor_for_seconds);
+
+    var obj = {
+        "h": hours,
+        "m": minutes,
+        "s": seconds
+    };
+    return obj;
+}
+
+function printTime(){
+	let hms = secondsToTime(secs);
+	let out = "";
+	if(hms.h>0){
+		out = out + hms.h + " st, ";
+	}
+	if(hms.m>0){
+		out = out + hms.m + " min, ";
+	}
+	if(hms.s>0){
+		out = out + hms.s + "s";
+	}
+	return out;
+	
+}
+
+function clickRaj(i){
+
+	console.log("Clicked: " + raj[i].name + " (" + raj[i].id + ")");
+	
+	
+	let click_id = raj[i].id; /// Nospiestā reģiona ID
+	let now_id = random[raj_nr].id; /// Vajadzīgās reģiona ID
+	let poly = getRegionPolygon(raj[i].id);
+	
+	
+	/// Pieskaitīt mēģinājumu
+	if(!raj[i].guessed){
+		random[raj_nr].attempts++;
+		raj[i].attempts++;
+		attempts++;
+	}
+
+	/// Pārbaudīt vai pareizi
+	if(click_id === now_id){		
+		/// Pareizi
+	
+		/// Atzīmēt atminējumu
+		random[raj_nr].guessed = true;
+		raj[i].guessed = true;
+
+		/// Iekrāsot atminēto		
+		markRegionAsGuessed(i);
+		
+		/// Pārbaudīt vai spēle beidzas
+		raj_nr++;
+		if(raj_nr < random.length){		
+			/// Ja ir reģioni, sākt nākamo	
+			printEmoji("🤩");	
+			printMsg("Pareizi! 🎉", "white");
+			winAnim();
+			setTimeout(() => {
+				printNextRegionQuestion();
+			}, 1000);
+		}else{		
+			/// Ja nav reģionu - spēles beigas
+			gameOver();
+		}
+		
+	}else{
+		/// Ja nepareizi, pārbaudīt vai šis jau ir atminēts un prasīt vēlreiz
+		poly.style.fill = "var(--red-color)";
+		
+		if(raj[i].guessed === true){
+			printEmoji("😅");
+			printMsg(`Jau atminēts - ${raj[i].name}!`, "white");
+			setTimeout(() => {
+			
+				markRegionAsGuessed(i);
+				printNextRegionQuestion();
+				
+			}, 1000);
+		}else{
+			
+			/// Pateikt, ka nepareizi
+			printEmoji("🥺");
+			printMsg(`Nepareizi - ${raj[i].name}!`, "white");
+			setTimeout(() => {
+			
+				if(raj[i].color){
+					poly.style.fill = raj[i].color;
+				}else{
+					poly.style.fill = "var(--light-green-color)";
+				}
+				printNextRegionQuestion();
+			}, 1000);
+		}
+	}
+	
+}
+
+function markRegionAsGuessed(i){
+
+	raj[i].guessed = true;
+
+	let poly = getRegionPolygon(raj[i].id);	
+	if(raj[i].color){
+		// poly.style.opacity = 0.5;
+	}else{
+		poly.style.fill = "var(--dark-green-color)";
+	}
+}
+
+
+function gameOver(){
+
+	timer_enabled = false;
+	let mistakes = attempts - raj_nr;
+	let mistakes_txt = (mistakes === 1 ? " kļūda" : " kļūdas");
+	let penalty = mistakes * 10;
+	let final_time = 10000 - secs + penalty;
+
+	printEmoji("🥳");
+	end_screen.style.display = "block";
+	end_screen.innerHTML = `
+		<center>
+			<br/>
+			<br/>
+			<br/>
+			<span id="win_emoji">🎉🎉🎉🏆🎉🎉🎉</span>
+			<h1>UZVARA!!!</h1>
+			<br/>Laiks ${secs} sekundes
+			<br/>+${penalty} sekundes (${mistakes} ${mistakes_txt})
+			<br/>
+			<br/>
+			<br/>
+				<h1>
+					Rezultāts: ${final_time} punkti
+				</h1>
+			<br/><a href='#'onclick='location.reload()'>Atkārtot</a>
+		</center>`;
+	winAnim("balloons2");
+
+	map.style.opacity = 0.5;
+}
+function printMsg(text, color){
+	let game_question = getById("game_question");
+	game_question.innerHTML = text;
+	
+	let guessed = document.getElementById("guessed");
+	guessed.innerHTML = raj_nr + " /" + raj.length;
+	
+	let mistakes = document.getElementById("mistakes");
+	mistakes.innerHTML = attempts - raj_nr;
+}
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function winAnim(animation_id="balloons"){
+
+	let mili = 5;
+	var elem = getById(animation_id);
+	elem.style.display = "flex";
+	var bottom_pos = -50;
+	var left_pos = 25;
+	var id = setInterval(frame, mili);
+	function frame() {
+		if (bottom_pos > 100) {
+			elem.style.display = "none";
+			clearInterval(id);		
+		} else {
+			bottom_pos = bottom_pos + 0.5;
+			left_pos = left_pos + (Math.random()-0.1)*0.1;
+			elem.style.bottom = bottom_pos + '%';
+			// elem.style.left = left_pos + '%';
+		}
+	}
+}
---
+++ geo/levels.js
@@ -0,0 +1,474 @@
+
+let levels = [
+	{
+		map_path: "map_1941.html",
+		regions: [
+			{
+				name: "Talsu rajons",
+				id: "raj_1"
+			},
+			{
+				name: "Kolkas rajons",
+				id: "raj_2"
+			},
+			{
+				name: "Rīgas rajons",
+				id: "raj_3"
+			},
+			{
+				name: "Saulkrastu rajons",
+				id: "raj_4"
+			},
+			{
+				name: "Ventspils",
+				id: "raj_5"
+			},
+			{
+				name: "Ventspils rajons",
+				id: "raj_6"
+			},
+			{
+				name: "Pāvilostas rajons",
+				id: "raj_7"
+			},
+			{
+				name: "Liepājas rajons",
+				id: "raj_8"
+			},
+			{
+				name: "Smiltenes rajons",
+				id: "raj_9"
+			},
+			{
+				name: "Valkas rajons",
+				id: "raj_10"
+			},
+			{
+				name: "Apes rajons",
+				id: "raj_11"
+			},
+			{
+				name: "Madonas rajons",
+				id: "raj_12"
+			},
+			{
+				name: "Varakļānu rajons",
+				id: "raj_13"
+			},
+			{
+				name: "Gulbenes rajons",
+				id: "raj_14"
+			},
+			{
+				name: "Viļānu rajons",
+				id: "raj_15"
+			},
+			{
+				name: "Abrenes rajons",
+				id: "raj_16"
+			},
+			{
+				name: "Dobeles rajons",
+				id: "raj_17"
+			},
+			{
+				name: "Jelgavas rajons",
+				id: "raj_18"
+			},
+			{
+				name: "Tērvetes rajons",
+				id: "raj_19"
+			},
+			{
+				name: "Jelgava",
+				id: "raj_20"
+			},
+			{
+				name: "Bauskas rajons",
+				id: "raj_21"
+			},
+			{
+				name: "Ainažu rajons",
+				id: "raj_22"
+			},
+			{
+				name: "Rūjienas rajons",
+				id: "raj_23"
+			},
+			{
+				name: "Valmieras rajons",
+				id: "raj_24"
+			},
+			{
+				name: "Cēsu rajons",
+				id: "raj_25"
+			},
+			{
+				name: "Siguldas rajons",
+				id: "raj_26"
+			},
+			{
+				name: "Baldones rajons",
+				id: "raj_27a"
+			},
+			{
+				name: "Ogres rajons",
+				id: "raj_27b"
+			},
+			{
+				name: "Rīga",
+				id: "raj_28"
+			},
+			{
+				name: "Limbažu rajons",
+				id: "raj_29"
+			},
+			{
+				name: "Kuldīgas rajons",
+				id: "raj_30"
+			},
+			{
+				name: "Aizputes rajons",
+				id: "raj_31"
+			},
+			{
+				name: "Liepāja",
+				id: "raj_32"
+			},
+			{
+				name: "Embūtes rajons",
+				id: "raj_33"
+			},
+			{
+				name: "Skrundas rajons",
+				id: "raj_34"
+			},
+			{
+				name: "Jaunjelgavas rajons",
+				id: "raj_35"
+			},
+			{
+				name: "Neretas rajons",
+				id: "raj_36"
+			},
+			{
+				name: "Jēkabpils rajons",
+				id: "raj_37"
+			},
+			{
+				name: "Krustpils rajons",
+				id: "raj_38"
+			},
+			{
+				name: "Pļaviņu rajons",
+				id: "raj_39"
+			},
+			{
+				name: "Aizkraukles rajons",
+				id: "raj_40"
+			},
+			{
+				name: "Ērgļu rajons",
+				id: "raj_41"
+			},
+			{
+				name: "Vecpiebalgas rajons",
+				id: "raj_42"
+			},
+			{
+				name: "Saldus rajons",
+				id: "raj_43"
+			},
+			{
+				name: "Kandavas rajons",
+				id: "raj_44"
+			},
+			{
+				name: "Tukuma rajons",
+				id: "raj_45"
+			},
+			{
+				name: "Auces rajons",
+				id: "raj_46"
+			},
+			{
+				name: "Aknīstes rajons",
+				id: "raj_47"
+			},
+			{
+				name: "Daugavpils rajons",
+				id: "raj_48"
+			},
+			{
+				name: "Daugavpils",
+				id: "raj_49"
+			},
+			{
+				name: "Nīcgales rajons",
+				id: "raj_50"
+			},
+			{
+				name: "Līvānu rajons",
+				id: "raj_51"
+			},
+			{
+				name: "Preiļu rajons",
+				id: "raj_52"
+			},
+			{
+				name: "Alūksnes rajons",
+				id: "raj_53"
+			},
+			{
+				name: "Balvu rajons",
+				id: "raj_54"
+			},
+			{
+				name: "Barkavas rajons",
+				id: "raj_55"
+			},
+			{
+				name: "Rēzeknes rajons",
+				id: "raj_56"
+			},
+			{
+				name: "Krāslavas rajons",
+				id: "raj_57"
+			},
+			{
+				name: "Maltas rajons",
+				id: "raj_58"
+			},
+			{
+				name: "Dagdas rajons",
+				id: "raj_59"
+			},
+			{
+				name: "Zilupes rajons",
+				id: "raj_60"
+			},
+			{
+				name: "Ludzas rajons",
+				id: "raj_61"
+			},
+			{
+				name: "Bikavas rajons",
+				id: "raj_62"
+			},
+			{
+				name: "Roņu sala",
+				id: "raj_63"
+			},
+			{
+				name: "Sāremā sala",
+				id: "raj_64"
+			},
+			{
+				name: "Kihnu sala",
+				id: "raj_65"
+			},
+			{
+				name: "Lietuva",
+				id: "raj_66"
+			},
+			{
+				name: "Baltijas Jūra",
+				id: "raj_67",
+				color: "var(--light-blue-color)"
+			},
+			{
+				name: "Igaunija",
+				id: "raj_68"
+			},
+			{
+				name: "Krievija",
+				id: "raj_69"
+			},
+			{
+				name: "Baltkrievija",
+				id: "raj_70"
+			}
+			
+		]
+	},
+	{
+		map_path: "map_2021.html",
+		regions: [
+			{
+				name: "Daugavpils",
+				id: "path4128"
+			},
+			{
+				name: "Augšdaugavas novads",
+				id: "path3856"
+			},
+			{
+				name: "Krāslavas novads",
+				id: "path3858"
+			},
+			{
+				name: "Ludzas novads",
+				id: "path3860"
+			},
+			{
+				name: "Alūksnes novads",
+				id: "path3862"
+			},
+			{
+				name: "Gulbenes novads",
+				id: "path3864"
+			},
+			{
+				name: "Balvu novads",
+				id: "path3866"
+			},
+			{
+				name: "Rēzekne",
+				id: "path4130"
+			},
+			{
+				name: "Rēzeknes novads",
+				id: "path3870"
+			},
+			{
+				name: "Preiļu novads",
+				id: "path3872"
+			},
+			{
+				name: "Līvānu novads",
+				id: "path3874"
+			},
+			{
+				name: "Varakļānu novads",
+				id: "path3876"
+			},
+			{
+				name: "Madonas novads",
+				id: "path3878"
+			},
+			{
+				name: "Jēkabpils novads",
+				id: "path3880"
+			},
+			{
+				name: "Aizkraukles novads",
+				id: "path3882"
+			},
+			{
+				name: "Ogres novads",
+				id: "path3884"
+			},
+			{
+				name: "Cēsu novads",
+				id: "path3886"
+			},
+			{
+				name: "Smiltenes novads",
+				id: "path3888"
+			},
+			{
+				name: "Valkas novads",
+				id: "path3890"
+			},
+			{
+				name: "Valmieras novads",
+				id: "path3892"
+			},
+			{
+				name: "Limbažu novads",
+				id: "path3894"
+			},
+			{
+				name: "Siguldas novads",
+				id: "path3896"
+			},
+			{
+				name: "Saulkrastu novads",
+				id: "path3898"
+			},
+			{
+				name: "Ādažu novads",
+				id: "path3900"
+			},
+			{
+				name: "Ropažu novads",
+				id: "path3902"
+			},
+			{
+				name: "Salaspils novads",
+				id: "path3904"
+			},
+			{
+				name: "Bauskas novads",
+				id: "path3906"
+			},
+			{
+				name: "Ķekavas novads",
+				id: "path3908"
+			},
+			{
+				name: "Olaines novads",
+				id: "path3910"
+			},
+			{
+				name: "Rīga",
+				id: "path3912"
+			},
+			{
+				name: "Mārupes novads",
+				id: "path3914"
+			},
+			{
+				name: "Jūrmala",
+				id: "path3916"
+			},
+			{
+				name: "Jelgava",
+				id: "path3918"
+			},
+			{
+				name: "Jelgavas novads",
+				id: "path3920"
+			},
+			{
+				name: "Dobeles novads",
+				id: "path3922"
+			},
+			{
+				name: "Saldus novads",
+				id: "path3924"
+			},
+			{
+				name: "Tukuma novads",
+				id: "path3926"
+			},
+			{
+				name: "Talsu novads",
+				id: "path3928"
+			},
+			{
+				name: "Ventspils",
+				id: "path3930"
+			},
+			{
+				name: "Ventspils novads",
+				id: "path3932"
+			},
+			{
+				name: "Kuldīgas novads",
+				id: "path3934"
+			},
+			{
+				name: "Liepāja",
+				id: "path3936"
+			},
+			{
+				name: "Dienvidkurzemes novads",
+				id: "path3938"
+			},
+			
+			
+
+		]
+	}
+]
